8c4d0ad16d1569a65f33e3a93959880561182574
Bertrand Marron <bertrand.marron@ionis-group.com>
2014-08-11 12:34:48 +0200

    Adding Watch and Matchdep, cleaning useless tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,33 @@
 'use strict';
 
 module.exports = function (grunt) {
-    require('load-grunt-tasks')(grunt);
 
-    grunt.initConfig({
-        jshint: {
-            all: [
-                'Gruntfile.js',
-                'tasks/*.js'
-            ],
-            options: {
-                jshintrc: '.jshintrc',
-            }
-        }
-    });
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js'
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+      }
+    },
 
-    grunt.registerTask('test', [
-        'jshint'
-    ]);
+    watch: {
+      jsValidate: {
+        files: [
+          'Gruntfile.js',
+          'tasks/*.js'
+        ],
+        tasks: ['jshint']
+      }
+    }
+  });
+  // Load the plugins
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-    // Alias default task to test
-    grunt.registerTask('default', [
-        'test'
-    ]);
+  grunt.registerTask('default', ['watch']);
+
+
 };
+
